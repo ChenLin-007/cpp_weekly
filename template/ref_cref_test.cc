4035f1935a6d6941a09ca0e657d47f7877f37218
CL <48561979+ChenLin-007@users.noreply.github.com>
2022-10-10 16:28:18 +0800

    Test: std::ref and std::cref

--- template/ref_cref_test.cc
+++ template/ref_cref_test.cc
@@ -14,7 +14,7 @@ void f(int &n1, int &n2, const int &n3) {
 
 TEST(ref_cref_test, basic_test) {
   int n1 = 1, n2 = 2, n3 = 3;
-  std::function<void()> bound_f = std::bind(f, n1, std::ref(n2), std:cref(n3));
+  std::function<void()> bound_f = std::bind(f, n1, std::ref(n2), std::cref(n3));
 
   n1 = 10;
   n2 = 11;
