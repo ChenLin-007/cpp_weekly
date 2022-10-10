a62954b15270d1358f75517cc794cfdb7e1f1b35
CL <48561979+ChenLin-007@users.noreply.github.com>
2022-10-10 16:49:23 +0800

    Test:: variadic templates

---
+++ template/chap4_variadic_templates.cc
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+template<typename T>
+void print(T arg) { std::cout << arg << '\n'; }
+
+template<typename T, typename... Types>
+void print(T firstArg, Types... args) {
+  print(firstArg);
+  print(args...);
+}
+
+TEST(chap4_variadic_templates, print_test) {
+  std::stringstream oss;
+
+  // start to capture stdout.
+  testing::internal::CaptureStdout();
+
+  print("Hello", "world");
+  oss << "Hello\nworld\n";
+
+  std::string act_output = testing::internal::GetCapturedStdout();
+
+  EXPECT_TRUE(oss.str(), act_output);
+}
