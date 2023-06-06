6fe3e0d9faefa2546cc516a56655a43833ca6f59
CL <chenlin048830@gmail.com>
2023-06-06 22:03:41 +0800

    add C++11: decltype.

--- template/function_basic.cc
+++ template/function_basic.cc
@@ -6,7 +6,19 @@ constexpr T add(T a, T b) {
 	return a + b;
 }
 
+// C++11 使用decltype辅助推导返回类型。
+template<typename T, typename U>
+auto decladd(T const & a, U const & b) -> decltype(a + b)
+{
+	return a + b;
+}
+
 TEST(BASIC, constexpr) {
 	auto sum = add(1, 2);
 	EXPECT_EQ(sum, 3);
+}
+
+TEST(BASIC, decltype) {
+	auto re = decladd(1, 3.0);
+	std::cout << "type: " << typeid(re).name() << std::endl;
 }
